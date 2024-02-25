2c0070c1a6b0d791629f5e6a2b9df2ca660023cf
manju0707 <manjunathasundi07@gmail.com>
2024-02-25 08:55:56 +0530

    Sum of factors of N

---
+++ lld-and-hld/src/main/java/com/lld/and/hld/lldandhld/lld/SumOfFactorsOfN.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ write code to find sum of factors of the input number.
+ 
+ N = 10, {1, 2, 5, 10} = 18
+ N = 9,  {1, 3, 9} = 13
+ 
+ 1 to N => N%i == 0 , sum += i;
+ 
+ // O(logN)
+ 1 to sqrt(N) => N%i == 0 , sum += i;
+ 
+ sum += N; 
+ */
+
+package com.lld.and.hld.lldandhld.lld;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * SumOfFactorsOfN
+ */
+public class SumOfFactorsOfN {
+
+    static int sumOfFactorsOfN(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        Set<Integer> set = new TreeSet<>();
+
+        int sum = 0;
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+
+            if (n % i == 0) {
+
+                set.add(i);
+
+                if ((n / i) == i) {
+                    sum += i;
+                } else {
+                    sum += (i + (n / i));
+                    set.add(n / i);
+                }
+            }
+        }
+
+        System.out.println(set);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfFactorsOfN(0));
+        System.out.println(sumOfFactorsOfN(1));
+        System.out.println(sumOfFactorsOfN(10));
+        System.out.println(sumOfFactorsOfN(9));
+        System.out.println(sumOfFactorsOfN(100));
+        System.out.println(sumOfFactorsOfN(200));
+    }
+}
