044e226ef7e246ada001c5a3c5225703d3b29ddb
m0a07xh <manjunath.asundi@walmart.com>
2024-07-24 10:20:24 +0530

    JPA with hibernate notes file updated

--- src/main/java/com/lld/and/hld/lldandhld/notes/JPA with Hibernate.dart
+++ src/main/java/com/lld/and/hld/lldandhld/notes/JPA with Hibernate.dart
@@ -37,20 +37,25 @@
     - ManyToOne
     - ManyToMany
 
-- Cascade Types (https://www.baeldung.com/jpa-cascade-types)
+- Cascade Types (https://www.baeldung.com/jpa-cascade-types) - this is used for how to behave on child entities.
     - JPA Types
-        - all
-        - persist
-        - merge
-        - remove
-        - detach
-        - refresh
+        - ALL - Perform all operations from parent to child entities
+        - PERSIST - Perform save operations from parent to child entities
+        - MERGE - Perform update operations from parent to child entities
+        - REMOVE - Perform delete operations from parent to child entities
+        - DETACH - Perform delete entity operations from persistent context from parent to child entities.
+        - REFRESH - Perform reloading/rereading the data from DB from parent to child entities.
 
     - Hibernate Types
-        - delete
-        - replicate
-        - save_update
-        - lock
+        - delete - same as REMOVE
+        - replicate 
+        - save_update 
+        - LOCK -  Perform attach entity operations to persistent context  (opposite of DETACH).
+
+    Ex:
+    @Column(cascade=CascadeType.ALL)
+
+
 
 - Persistence Context (https://www.baeldung.com/jpa-hibernate-persistence-context#test_cases_extended_persistence_context)
     - Using EntityManager can do CRUD operations with entities.
@@ -60,7 +65,10 @@
 
         - Flow:
             - application -> persistence context -> DB
-
+    
+    - Persistence Context is a cache which entities saved/updated to DB.
+        - Hibernate provides the Session interface for persistence context (https://www.baeldung.com/hibernate-save-persist-update-merge-saveorupdate)
+        - JPA provides the EntityManager interface for persistence context.
 
     - Types
         - Transaction Persistence Context(Default):  available to one transaction. after finishing the transaction, entities pushed/flushed to DB.
@@ -74,4 +82,70 @@
             private EntityManager entityManager;
             
             
+
+- Types of JPA Queries (https://www.baeldung.com/jpa-queries)
+
+    - JPQL(Java persistence query language): execute on entities, similar to native SQL queries
+
+        Ex:
+        Employee getEmployeeById(long id) {
+            Query query = entityManager.createQuery("select E from Employee e where e.id=:id");
+            query.setParameter("id", id);
+            return  (Employee) query.getSingleResult();
+        }
+
+        - Types:
+            - Typed Query: Specify the expected output while creating query
+                Employee getEmployeeById(long id) {
+                    TypedQuery<Employee> typedQuery = entityManager.createQuery("select e from Employee e where e.id=:id", Employee.class);
+                    typedQuery.setParameter("id", id);
+                    return typedQuery.getSingleResult();
+                }
+
+            - Named Query: Specify in entity level, this is a static. use the named query name to fetch the query.
+                @Entity
+                @NamedQuery(name ="fetch_employee_by_id", query = "select e from Employee e where e.id=:id")
+                class Employee {
+
+                }
+
+                Employee getEmployeeById(long id) {
+                    Query namedQuery = entityManager.createNamedQuery("fetch_employee_by_id");
+                    namedQuery.setParameter("id", id);
+                    return (Employee) namedQuery.getSingleResult();
+                }
+
+
+    - Native Query: Execute raw SQL queries.
+        
+        Ex:
+
+        Employee getEmployeeById(long id) {
+            Query nativeQuery = entityManager.createNativeQuery("select * from employee e where e.id=:id");
+            nativeQuery.setParameter("id", id);
+            return  (Employee) nativeQuery.getSingleResult();
+        }
+
+    - Criteria Query: programmitically built, type-safe.
+
+- JPA Query Params (https://www.baeldung.com/jpa-query-parameters)
+
+    - Positional Parameters
+        Employee getEmployeeById(long id, String deptName) {
+            Query query = entityManager.createQuery("select E from Employee e where e.id=?1 and e.deptName=?2");
+            query.setParameter(1, id);
+            query.setParameter(2, deptName);
+            return  (Employee) query.getSingleResult();
+        }
+
+
+
+    - Named Parameters
+        Employee getEmployeeById(long id, String deptName) {
+            Query query = entityManager.createQuery("select E from Employee e where e.id=:id and e.deptName=:deptName");
+            query.setParameter("id", id);
+            query.setParameter("deptName", deptName);
+            return  (Employee) query.getSingleResult();
+        }
+
 
